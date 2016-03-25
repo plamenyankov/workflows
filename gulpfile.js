6e10c5a51a7bdc452051ccc6d3563b75d942c306
Plamen Yankov <p.s.yankov@abv.bg>
2016-03-25 19:13:58 +0200

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,33 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 var jsSources = [
-  'components/scripts/rclick.js',
-  'components/scripts/pixgrid.js',
-  'components/scripts/tagline.js',
-  'components/scripts/template.js'
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
 ];
-gulp.task('coffee',function(){
-   gulp.src('components/coffee/tagline.coffee')
-       .pipe(coffee({bare:true})
-           .on('error',gutil.log)
-           ).pipe(gulp.dest('components/scripts'))
+var cssSources = 'components/sass/style.scss';
+gulp.task('coffee', function () {
+    gulp.src('components/coffee/tagline.coffee')
+        .pipe(coffee({bare: true})
+            .on('error', gutil.log)
+    ).pipe(gulp.dest('components/scripts'))
 });
-gulp.task('js',function() {
-gulp.src(jsSources)
-    .pipe(concat('scripts.js'))
-    .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'))
+gulp.task('js', function () {
+    gulp.src(jsSources)
+        .pipe(concat('scripts.js'))
+        .pipe(browserify())
+        .pipe(gulp.dest('builds/development/js'))
+});
+gulp.task('compass', function () {
+    gulp.src(cssSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            images: 'build/development/images',
+            style: 'expanded'
+        }).on('error', gutil.log))
+        .pipe(gulp.dest('builds/development/css'))
 });
